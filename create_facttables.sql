47bab07f8e9bf908da3ed3d85fea9fcef7222860
Kean Scott <keanscott01@gmail.com>
2025-04-06 08:35:56 +0800

    Add create_facttables.sql. Creates SQL tables for storing fact tables via PostgreSQL

---
+++ create_facttables.sql
@@ -0,0 +1,24 @@
+CREATE TABLE factcrash (
+	crash_id INTEGER PRIMARY KEY,
+	location_id INTEGER,
+	time_id INTEGER,
+	crashdetails_id INTEGER,
+	numberfatalities INTEGER,
+	FOREIGN KEY (location_id) REFERENCES dimlocation(location_id),
+	FOREIGN KEY (time_id) REFERENCES dimtime(time_id),
+	FOREIGN KEY (crashdetails_id) REFERENCES dimcrashdetails(crashdetails_id)
+);
+
+CREATE TABLE factfatality (
+	fatality_id INTEGER PRIMARY KEY,
+	crash_id INTEGER,
+	person_id INTEGER, 
+	location_id INTEGER,
+	time_id INTEGER,
+	crashdetails_id INTEGER,
+	FOREIGN KEY (crash_id) REFERENCES factcrash(crash_id),
+	FOREIGN KEY (person_id) REFERENCES dimpersondetails(person_id),
+	FOREIGN KEY (location_id) REFERENCES dimlocation(location_id),
+	FOREIGN KEY (time_id) REFERENCES dimtime(time_id),
+	FOREIGN KEY (crashdetails_id) REFERENCES dimcrashdetails(crashdetails_id)
+);
